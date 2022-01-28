46c11e05aec604a45f043ae4e04230daff243d78
dmogilevsky <mogilevsky.daniel@gmail.com>
2022-01-28 00:44:27 -0500

    Pre-dev additional logic improvements

--- lab-1/SetGameLogic.js
+++ lab-1/SetGameLogic.js
@@ -1,5 +1,6 @@
 // 81 Cards
 //Each card has 4 attributes
+
 class Card {
     constructor(id, shade, shape, color, number) {
         this.id = id; // 1-81
@@ -10,12 +11,17 @@ class Card {
     }
 }
 
+let GameBoard = []; // Holds the 12 visible cards
+let Deck = [];  // The deck of 81 cards
+
 // Do all the necessary initialization
 function StartGame() {
     let AttributeMapping = makeAttributeMappings();
-    let Cards = initializeCards(AttributeMapping);
-    let Deck = createDeck(Cards);
+    initializeDeck(AttributeMapping);
+    shuffleDeck();
+    createGameBoard();
 }
+
 /* Cards are ordered by:
    Shade: Bold, Striped, Hollow
        Shape: Squiggle, Diamond, Oval
@@ -23,58 +29,46 @@ function StartGame() {
              Number: 1, 2, 3
 Make mapping for shade, shape, color relative to card order */
 function makeAttributeMappings() {
-    const maps = [
-        new Map(), // Shade
-        new Map(), // Shape
-        new Map(), // Color
+    return [
+        ["Bold", "Striped", "Hollow"], // 0. Shade
+        ["Squiggle", "Diamond", "Oval"], // 1. Shape
+        ["Red", "Purple", "Green"], // 2. Color
     ];
-    maps[0].set(1, "Bold");
-    maps[0].set(2, "Striped");
-    maps[0].set(3, "Hollow");
-    maps[1].set(1, "Squiggle");
-    maps[1].set(2, "Diamond");
-    maps[1].set(3, "Oval");
-    maps[2].set(1, "Red");
-    maps[2].set(2, "Purple");
-    maps[2].set(3, "Green");
-    return maps;
 }
 
 // Make an array of all 81 cards
-function initializeCards(map) {
-    const Cards = [];
-    let idCount = 1;
-    for (let i =1; i <= 3;i++) {
-        for (let j =1; j <= 3;j++) {
-            for (let k =1; k <= 3;k++) {
-                for (let l=1;l<=3;l++) {
-                    Cards[idCount-1] = new Card(idCount, map[0].get(i), map[1].get(j),map[2].get(k),l);
+function initializeDeck(map) {
+    let idCount = 1; // Corresponding with the #.jpg of the card
+    for (let i =1; i <= 3;i++) { // The shade of the card, Bold, Striped, or Hollow
+        for (let j =1; j <= 3;j++) { // The shape of the card, Squiggle, Diamond, Oval
+            for (let k =1; k <= 3;k++) { // The color, Red, Purple, Green
+                for (let l=1;l<=3;l++) { // Number of shapes in the card
+                    Deck[idCount-1] = new Card(idCount, map[0][i], map[1][j],map[2][k],l);
                     idCount++
                 }
             }
         }
     }
-    return Cards;
+    shuffleDeck();
 }
 
-// Simply shuffle the Cards array
-function createDeck(Cards) {
-    let curId = Cards.length;
+// Simply shuffle the Deck array
+function shuffleDeck() {
+    let curId = Deck.length;
     // There remain elements to shuffle
     while (0 !== curId) {
         // Pick a remaining element
         let randId = Math.floor(Math.random() * curId);
         curId -= 1;
         // Swap it with the current element.
-        let tmp = Cards[curId];
-        Cards[curId] = Cards[randId];
-        Cards[randId] = tmp;
+        let tmp = Deck[curId];
+        Deck[curId] = Deck[randId];
+        Deck[randId] = tmp;
     }
-    return Cards;
 }
 
 // Draw 3 cards to replace matched set and remove the cards from the deck
-function drawCards(Deck) {
+function drawCards() {
     let DrawnCards = [
         Deck.length -1 ,
         Deck.length - 2,
@@ -84,9 +78,11 @@ function drawCards(Deck) {
     return DrawnCards;
 }
 
-// Update DOM to start game
+// Update DOM to start game, needs 12 cards
 function createGameBoard() {
-    //drawCards x 4
+    for (let i=0;i<3;i++) {
+        GameBoard.push.apply(GameBoard, drawCards)
+    }
 }
 
 // A set is defined as follows:
@@ -96,12 +92,4 @@ function isSet(x, y, z) {
     (x.shade === y.shade === z.shade) || (x.shade !== y.shade && x.shade !== z.shade && y.shade !== z.shade) &&
     (x.number === y.number === z.number) || (x.number !== y.number && x.number !== z.number && y.number !== z.number) &&
     (x.shape === y.shape === z.shape) || (x.shape !== y.shape && x.shape !== z.shape && y.shape !== z.shape);
-}
-
-function isWon() {
-
-}
-
-function shuffle() {
-
 }
