1ae38af94acb04d9ecf861edaae9b0ca214add07
dmogilevsky <mogilevsky.daniel@gmail.com>
2022-03-22 19:34:04 -0400

    Changed user to student, created default admin user

--- lab-2/RubyRails/app/models/user.rb
+++ lab-2/RubyRails/app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum role: [:user, :instructor, :admin]
+  enum role: [:student, :instructor, :admin]
   after_initialize :set_role_default, :if => :new_record?
   def set_role_default
-    self.role ||= :user
+    self.role ||= :student
   end
+
+  # TODO add validation
 end
--- lab-2/RubyRails/db/seeds.rb
+++ lab-2/RubyRails/db/seeds.rb
@@ -8,27 +8,28 @@
 #
 require "httparty"
 require "active_record"
-#require "../app/models/course.rb"
-#require "../app/models/section.rb"
 url = "https://content.osu.edu/v2/classes/search?q=&subject=cse"
 page_data = HTTParty.get(url)
 response = OpenStruct.new(page_data.parsed_response) # Response is of type Hash, made into OpenStruct
 #puts response.data.keys
 #puts response.data["courses"] # Courses is an array of hashes
-puts "#########################Course keys######################"
-puts response.data["courses"][0]["course"].keys
-puts "#########################Section keys######################"
-puts response.data["courses"][0]["sections"][0].keys
+#puts "#########################Course keys######################"
+#puts response.data["courses"][0]["course"].keys
+#puts "#########################Section keys######################"
+#puts response.data["courses"][0]["sections"][0].keys
 response.data["courses"].each { |courseContainer| #Each "course" contains a course (hash) and sections (array)
   course = courseContainer["course"]
   newcourse = Course.new(Subject: course["subject"],CourseNumber: course["catalogNumber"],
                        CourseName: course["title"])
   newcourse.save
-  puts course["title"]
-  puts newcourse.id
+  #puts course["title"]
+  #puts newcourse.id
   courseContainer["sections"].each { |section|
     # puts section["courseId"] + " " + section["section"]
     Section.create!(SectionNumber: section["section"], course_id: newcourse.id, Campus: section["campus"], Term:
       section["term"])
   }
 }
+
+User.create!(email: "admin@osu.edu", password: "password",
+             role: 2)
