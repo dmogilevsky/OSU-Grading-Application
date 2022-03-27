510d52b5875c55909ba5624609cab0eb2cfd9669
Godich <wang.11634@osu.edu>
2022-03-27 03:14:30 -0400

    Sorting function implemented

--- lab-2/RubyRails/app/controllers/courses_controller.rb
+++ lab-2/RubyRails/app/controllers/courses_controller.rb
@@ -1,7 +1,9 @@
 class CoursesController < ApplicationController
   def index
-    @pagy, @courses = pagy(Course.all)
+    @pagy,@courses = pagy(Course.all.order(params[:sort]))
     @pagy,@sections= pagy(Section.all)
   end
+  end
 
-end
+
+
