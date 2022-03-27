ff56388f2a4c0efb4522b54948773d0629b8dc8b
dmogilevsky <mogilevsky.daniel@gmail.com>
2022-03-27 14:59:16 -0400

    Added Home page button to courses page

--- lab-2/RubyRails/app/controllers/sections_controller.rb
+++ lab-2/RubyRails/app/controllers/sections_controller.rb
@@ -1,1 +1,44 @@
+class SectionsController < ApplicationController
+  def delete_section
+    Section.delete(params[:id])
+    redirect_to(courses_path)
+  end
 
+  def create
+    new_section= params[:section]
+    Section.create(Term: new_section[:Term], course_id: new_section[:CourseID],
+    SectionNumber: new_section[:SectionNumber])
+    redirect_to(courses_path)
+  end
+
+  def edit
+    section = Section.find(params[:id])
+    respond_to do |format|
+      format.html { render :edit_sections, locals: { section: section } }
+    end
+  end
+
+  def update
+    section = Course.find(params[:id])
+    respond_to do |format|
+      format.html do
+        if course.update(params.require(:course).permit(:CourseName, :Subject, :CourseNumber, :Campus,
+                                                        :Career))
+          flash[:success] = 'Course updated successfully'
+          redirect_to(courses_path)
+        else
+          flash.now[:error] = 'Error: Course could not be updated'
+          render :edit, locals: { course: course }
+        end
+      end
+    end
+  end
+
+  def update_section
+    section = Section.find(params[:id])
+    section.Term = params[:term]
+    section.SectionNumber = params[:sectionnumber]
+    section.course_id = params[:course_id]
+    section.save
+  end
+  end
--- lab-2/RubyRails/config/routes.rb
+++ lab-2/RubyRails/config/routes.rb
@@ -7,13 +7,25 @@ Rails.application.routes.draw do
   #get 'student_page', to: 'pages#home'
   get 'admin', to: 'pages#admin'
   get 'users/:id/edit' => 'users#approve_user', as: 'approve_user'
+
+  # Courses
   get 'courses/add', to: 'courses#add_courses', as: 'add_course'
   get 'courses/:id/edit', to: 'courses#edit', as: 'edit_course'
   patch 'courses/:id', to: 'courses#update' # update (as needed)
   put 'courses/:id', to: 'courses#update' # update (full replacement)
   get '/courses/:id/delete' => 'courses#delete_course', as: 'delete_course'
+
+  # Sections
+  get 'sections/add', to: 'sections#add_sections', as: 'add_section'
+  get 'sections/:id/edit', to: 'sections#edit', as: 'edit_section'
+  patch 'sections/:id', to: 'sections#update' # update (as needed)
+  put 'sections/:id', to: 'sections#update' # update (full replacement)
+  get '/sections/:id/delete' => 'section#delete_section', as: 'delete_section'
+
+  # Resources
   resources :users
   resources :courses
+  resources :sections
   resources :admin_courses
 end
 
