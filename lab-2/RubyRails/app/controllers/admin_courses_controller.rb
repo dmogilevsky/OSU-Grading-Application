2a72cc656948af0ff9a2ad1c1d21f1e04d3f23da
Godich <wang.11634@osu.edu>
2022-03-27 14:57:15 -0400

    Search course done

--- lab-2/RubyRails/app/controllers/admin_courses_controller.rb
+++ lab-2/RubyRails/app/controllers/admin_courses_controller.rb
@@ -1,7 +1,15 @@
 class AdminCoursesController < ApplicationController
   def index
-    @pagy,@courses = pagy(Course.all.order(params[:sort]))
-    @pagy,@sections= pagy(Section.all)
+    @pagy, @courses = pagy(Course.all.order(params[:sort]))
+    @pagy, @sections = pagy(Section.all)
+
+    if params[:search]
+      search_courses
+    end
+  end
+
+  def search_courses
+    @pagy, @courses = pagy(Course.where("CourseNumber LIKE ?", "%" + params[:search]+ "%"))
   end
 
   # TODO add new course
--- lab-2/RubyRails/app/controllers/courses_controller.rb
+++ lab-2/RubyRails/app/controllers/courses_controller.rb
@@ -2,6 +2,14 @@ class CoursesController < ApplicationController
   def index
     @pagy, @courses = pagy(Course.all.order(params[:sort]))
     @pagy, @sections = pagy(Section.all)
+
+    if params[:search]
+      search_courses
+    end
+  end
+
+  def search_courses
+    @pagy, @courses = pagy(Course.where("CourseNumber LIKE ?", "%" + params[:search]+ "%"))
   end
 
   def delete_course
