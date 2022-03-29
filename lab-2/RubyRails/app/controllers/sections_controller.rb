691052a823b42de4c1d6c2a3c7dd591cdd180496
dmogilevsky <mogilevsky.daniel@gmail.com>
2022-03-28 22:44:59 -0400

    Edit course and sections working

--- lab-2/RubyRails/app/controllers/sections_controller.rb
+++ lab-2/RubyRails/app/controllers/sections_controller.rb
@@ -19,26 +19,18 @@ class SectionsController < ApplicationController
   end
 
   def update
-    section = Course.find(params[:id])
+    section = Section.find(params[:id])
     respond_to do |format|
       format.html do
-        if course.update(params.require(:course).permit(:CourseName, :Subject, :CourseNumber, :Campus,
-                                                        :Career))
-          flash[:success] = 'Course updated successfully'
+        if section.update(params.require(:section).permit(:SectionNumber, :Term))
+          flash[:success] = 'Section updated successfully'
           redirect_to(courses_path)
         else
-          flash.now[:error] = 'Error: Course could not be updated'
-          render :edit, locals: { course: course }
+          flash.now[:error] = 'Error: Section could not be updated'
+          render :edit, locals: { section: section }
         end
       end
     end
   end
 
-  def update_section
-    section = Section.find(params[:id])
-    section.Term = params[:term]
-    section.SectionNumber = params[:sectionnumber]
-    section.course_id = params[:course_id]
-    section.save
-  end
   end
