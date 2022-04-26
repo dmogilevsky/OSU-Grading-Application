a14ad6a02364afc178688aa131ae0b99db6478f8
dmogilevsky <mogilevsky.daniel@gmail.com>
2022-04-25 22:23:27 -0400

    Graderforms submission works

--- lab-3/app/controllers/Graderforms_controller.rb
+++ lab-3/app/controllers/Graderforms_controller.rb
@@ -1,65 +1,35 @@
 class GraderformsController < ApplicationController
 
   def delete_grader
-
-    Grader.delete(params[:id])
-
+    Graderform.delete(params[:id])
     redirect_to(admin_path)
-
   end
-
-
 
   def create
-
-    new_grader= params[:grader]
-
-    Grader.create(email: new_grader[:email], CourseNumber: new_grader[:CourseNumber], Grade: new_grader[:Grade], Availability: new_grader[:Availability])
-
-    redirect_to(admin_path)
-
+    new_graderform = params[:graderform]
+    Graderform.create(student_id: current_user.id, Form: new_graderform[:Form])
+    redirect_to(root_path)
   end
-
-
 
   def edit
-
-    grader = Grader.find(params[:id])
+    grader = Graderform.find(params[:id])
     respond_to do |format|
-
       format.html { render :edit_graders, locals: { grader: grader } }
-
     end
-
   end
 
-
-
   def update
-
-    grader = Grader.find(params[:id])
-
+    grader = Graderform.find(params[:id])
     respond_to do |format|
-
       format.html do
-
         if grader.update(params.require(:grader).permit(:CourseNumber, :Grader))
-
           flash[:success] = 'Grader updated'
-
           redirect_to(grader_path)
-
         else
-
           flash.now[:error] = 'Error: Grader could not be updated'
-
           render :edit, locals: { grader: grader }
-
         end
-
       end
-
     end
-
   end
 end
